a07dddb0ef85735c7f7479e42c71042cad9227e1
Juan Ignacio Costamagna <juan.costamagna@mercadolibre.com>
2022-07-13 12:47:45 -0300

    Add solution

--- src/main/java/com/mercadolibre/dojos/Solution.java
+++ src/main/java/com/mercadolibre/dojos/Solution.java
@@ -1,3 +1,76 @@
 package com.mercadolibre.dojos;
 
-public class Solution {}
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Solution {
+  public int delete_distance(String str1, String str2) {
+    if (str1.equals(str2)) {
+      return 0;
+    }
+
+    Integer distance = 0;
+
+    StringBuffer string1 = new StringBuffer(str1);
+    StringBuffer string2 = new StringBuffer(str2);
+
+    String filteredString1 = this.deleteExtraCharactersFromFirstString(string1, string2);
+    distance += str1.length() - filteredString1.length();
+    String filteredString2 = this.deleteExtraCharactersFromFirstString(string2, string1);
+    distance += str2.length() - filteredString2.length();
+
+    if (filteredString1.equals(filteredString2)) {
+      return distance;
+    }
+
+    return distance + this.removeDuplicated(filteredString1, filteredString2);
+  }
+
+  private int removeDuplicated(String string1, String string2) {
+    return this.levenshteinDistance(string1, string2);
+  }
+
+  private int levenshteinDistance(String string1, String string2) {
+    int[][] matrix = new int[string1.length()][string2.length()];
+
+    Arrays.stream(matrix)
+        .forEach((int[] array) -> Arrays.fill(array, string1.length() + string2.length()));
+    matrix[0][0] = 0;
+
+    for (int i = 0; i < string1.length(); i++) {
+      for (int j = 0; j < string2.length(); j++) {
+        int jMinusOne = j == 0 ? 0 : j - 1;
+        int iMinusOne = i == 0 ? 0 : i - 1;
+
+        int distance =
+            Math.min(
+                Math.min(matrix[i][jMinusOne], matrix[iMinusOne][j]), matrix[iMinusOne][jMinusOne]);
+
+        if (string1.charAt(i) != string2.charAt(j)) {
+          distance += 1;
+        }
+
+        matrix[i][j] = distance;
+      }
+    }
+
+    return matrix[string1.length() - 1][string2.length() - 1];
+  }
+
+  private String deleteExtraCharactersFromFirstString(StringBuffer string1, StringBuffer string2) {
+    String str1 = string1.toString();
+    ArrayList<Integer> indexes = new ArrayList();
+    for (int i = 0; i < str1.length(); i++) {
+      if (string2.toString().indexOf(str1.charAt(i)) == -1) {
+        indexes.add(i);
+      }
+    }
+    Collections.reverse(indexes);
+    for (Integer index : indexes) {
+      string1 = string1.deleteCharAt(index);
+    }
+
+    return string1.toString();
+  }
+}
--- src/test/java/com/mercadolibre/dojos/DojoTest.java
+++ src/test/java/com/mercadolibre/dojos/DojoTest.java
@@ -14,12 +14,79 @@ public class DojoTest {
   public void test_case_00() {
 
     Solution solution = new Solution();
-    Assert.assertEquals(1, 1);
+
+    int result = solution.delete_distance("at", "cat");
+
+    Assert.assertEquals(1, result);
   }
 
-  @Test(expected = RuntimeException.class)
+  @Test
   public void test_case_01() {
 
-    throw new RuntimeException();
+    Solution solution = new Solution();
+
+    int result = solution.delete_distance("cat", "cat");
+
+    Assert.assertEquals(0, result);
+  }
+
+  @Test
+  public void test_case_02() {
+
+    Solution solution = new Solution();
+
+    int result = solution.delete_distance("boat", "got");
+
+    Assert.assertEquals(3, result);
+  }
+
+  @Test
+  public void test_case_03() {
+
+    Solution solution = new Solution();
+
+    int result = solution.delete_distance("thought", "sloughs");
+
+    Assert.assertEquals(6, result);
+  }
+
+  @Test
+  public void test_case_04() {
+
+    Solution solution = new Solution();
+
+    int result = solution.delete_distance("ttt", "aaa");
+
+    Assert.assertEquals(6, result);
+  }
+
+  @Test
+  public void test_case_05() {
+
+    Solution solution = new Solution();
+
+    int result = solution.delete_distance("tatata", "atatat");
+
+    Assert.assertEquals(2, result);
+  }
+
+  @Test
+  public void test_case_06() {
+
+    Solution solution = new Solution();
+
+    int result = solution.delete_distance("abcd", "cbad");
+
+    Assert.assertEquals(4, result);
+  }
+
+  @Test
+  public void test_case_07() {
+
+    Solution solution = new Solution();
+
+    int result = solution.delete_distance("hhhough", "ough");
+
+    Assert.assertEquals(3, result);
   }
 }
