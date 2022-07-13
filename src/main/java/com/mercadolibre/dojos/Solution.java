fb7ed2424cb80c73547f3f9ee43fca032746008b
Juan Ignacio Costamagna <juan.costamagna@mercadolibre.com>
2022-07-13 13:51:36 -0300

    add solution

--- src/main/java/com/mercadolibre/dojos/Solution.java
+++ src/main/java/com/mercadolibre/dojos/Solution.java
@@ -1,3 +1,39 @@
 package com.mercadolibre.dojos;
 
-public class Solution {}
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Solution {
+  public int findMistake(List<Integer> integers) {
+
+    int duplicatedInt = this.findDuplicated(integers);
+
+    int missingInt = this.findMissing(integers);
+
+    return duplicatedInt + missingInt;
+  }
+
+  private int findMissing(List<Integer> integers) {
+    List<Integer> integerStream =
+        IntStream.rangeClosed(1, integers.size()).boxed().collect(Collectors.toList());
+
+    for (Integer integer : integers) {
+      integerStream.remove(integer);
+    }
+
+    return integerStream.stream().findFirst().orElse(0);
+  }
+
+  private int findDuplicated(List<Integer> integers) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (Integer integer : integers) {
+      if (map.containsKey(integer)) {
+        return integer;
+      }
+      map.put(integer, 1);
+    }
+    return 0;
+  }
+}
--- src/test/java/com/mercadolibre/dojos/DojoTest.java
+++ src/test/java/com/mercadolibre/dojos/DojoTest.java
@@ -1,5 +1,6 @@
 package com.mercadolibre.dojos;
 
+import java.util.List;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,12 +15,53 @@ public class DojoTest {
   public void test_case_00() {
 
     Solution solution = new Solution();
-    Assert.assertEquals(1, 1);
+
+    int result = solution.findMistake(List.of(1, 3, 3, 4));
+    Assert.assertEquals(5, result);
   }
 
-  @Test(expected = RuntimeException.class)
+  @Test
   public void test_case_01() {
 
-    throw new RuntimeException();
+    Solution solution = new Solution();
+
+    int result = solution.findMistake(List.of(4, 3, 3, 1));
+    Assert.assertEquals(5, result);
+  }
+
+  @Test
+  public void test_case_02() {
+
+    Solution solution = new Solution();
+
+    int result = solution.findMistake(List.of(1, 1, 3, 4));
+    Assert.assertEquals(3, result);
+  }
+
+  @Test
+  public void test_case_03() {
+
+    Solution solution = new Solution();
+
+    int result = solution.findMistake(List.of(2, 2));
+    Assert.assertEquals(3, result);
+  }
+
+  @Test
+  public void test_case_04() {
+
+    Solution solution = new Solution();
+
+    int result = solution.findMistake(List.of(6, 3, 2, 4, 3, 1));
+    Assert.assertEquals(8, result);
+  }
+
+  @Test
+  public void test_case_05() {
+
+    Solution solution = new Solution();
+
+    int result = solution.findMistake(List.of(10, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    Assert.assertEquals(11, result);
   }
 }
