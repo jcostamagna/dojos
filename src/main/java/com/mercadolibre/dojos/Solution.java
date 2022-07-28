9e4373c5cdab98d72a314259fb9de8b6e05aaa02
Juan Ignacio Costamagna <costamagna.jic@gmail.com>
2022-07-28 12:18:27 -0300

    add solution

--- src/main/java/com/mercadolibre/dojos/Solution.java
+++ src/main/java/com/mercadolibre/dojos/Solution.java
@@ -1,3 +1,33 @@
 package com.mercadolibre.dojos;
 
-public class Solution {}
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public ArrayList<Integer> findSumIntegers(List<Integer> integers, int target) {
+
+        List<Integer> sortedIntegers = integers.stream().sorted().collect(Collectors.toList());
+
+        for (int i = 0; i < sortedIntegers.size() - 1; i++) {
+            List<Integer> subList = sortedIntegers.subList(i + 1, integers.size());
+
+            boolean integerFound = this.findInteger(subList, target - sortedIntegers.get(i));
+
+            if (integerFound) {
+                int firstIndex = integers.indexOf(sortedIntegers.get(i));
+                int lastIndex = integers.lastIndexOf(target - sortedIntegers.get(i));
+
+                return new ArrayList<>(List.of(firstIndex, lastIndex));
+            }
+
+        }
+
+        return new ArrayList<>();
+    }
+
+    private boolean findInteger(List<Integer> subList, int target) {
+        return Collections.binarySearch(subList, target) >= 0;
+    }
+}
--- src/test/java/com/mercadolibre/dojos/DojoTest.java
+++ src/test/java/com/mercadolibre/dojos/DojoTest.java
@@ -4,22 +4,69 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /** Tests for the dojo. */
 public class DojoTest {
 
   @Before
   public void setup() {}
 
+  /*
+  Given a list of integers nums and an integer target, return indices of the 2 numbers such that they add up to target
+  You may not use the same number twice
+
+  Input: nums = [3,2,4], target = 6
+  Output: [1,2]
+
+  Input: nums = [3,2,3], target = 6
+  Output: [0,2]
+
+  Input: nums = [3,2,1], target = 6
+  Output: []
+   */
+
   @Test
   public void test_case_00() {
 
     Solution solution = new Solution();
-    Assert.assertEquals(1, 1);
+
+    ArrayList<Integer> indexes = solution.findSumIntegers(List.of(3,2,4), 6);
+
+    ArrayList<Integer> expectedIndexes = new ArrayList<>();
+
+    expectedIndexes.add(1);
+    expectedIndexes.add(2);
+
+    Assert.assertEquals(expectedIndexes, indexes);
   }
 
-  @Test(expected = RuntimeException.class)
+  @Test
   public void test_case_01() {
 
-    throw new RuntimeException();
+    Solution solution = new Solution();
+
+    ArrayList<Integer> indexes = solution.findSumIntegers(List.of(3,2,3), 6);
+
+    ArrayList<Integer> expectedIndexes = new ArrayList<>();
+
+    expectedIndexes.add(0);
+    expectedIndexes.add(2);
+
+    Assert.assertEquals(expectedIndexes, indexes);
   }
+
+  @Test
+  public void test_case_02() {
+
+    Solution solution = new Solution();
+
+    ArrayList<Integer> indexes = solution.findSumIntegers(List.of(3,2,1), 6);
+
+    ArrayList<Integer> expectedIndexes = new ArrayList<>();
+
+    Assert.assertEquals(expectedIndexes, indexes);
+  }
+
 }
