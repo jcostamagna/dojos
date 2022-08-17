d4f0d92335b62ed6ddd6740df562eccefacf9269
Juan Ignacio Costamagna <costamagna.jic@gmail.com>
2022-08-17 16:47:03 -0300

    solution

--- src/main/java/com/mercadolibre/dojos/Solution.java
+++ src/main/java/com/mercadolibre/dojos/Solution.java
@@ -1,3 +1,25 @@
 package com.mercadolibre.dojos;
 
-public class Solution {}
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public List<String> findSecondLineProducts(ArrayList<Product> productArrayList) {
+        HashMap<Integer, List<String>> inventary = new HashMap<>();
+
+        productArrayList.stream().forEach(product -> {
+            List<String> actualProducts = inventary.getOrDefault(product.getPrice(), new ArrayList<>());
+
+            actualProducts.add(product.getName());
+
+            inventary.put(product.getPrice(), actualProducts);
+        });
+
+        List<Integer> keys = inventary.keySet().stream().sorted().collect(Collectors.toList());
+
+
+        Collections.reverse(keys);
+
+        return inventary.get(keys.get(1));
+    }
+}
--- src/test/java/com/mercadolibre/dojos/DojoTest.java
+++ src/test/java/com/mercadolibre/dojos/DojoTest.java
@@ -4,17 +4,43 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /** Tests for the dojo. */
 public class DojoTest {
 
   @Before
   public void setup() {}
 
+  /*
+  Product -> String name, int price
+  “Table”, 400
+  “Chair”, 200
+  “Tablet”, 300
+  “TV”, 400
+  “Phone”, 300
+  “keyboard,” 100
+  segundosMasCaros
+   */
   @Test
   public void test_case_00() {
 
     Solution solution = new Solution();
-    Assert.assertEquals(1, 1);
+
+    ArrayList<Product> productArrayList = new ArrayList<>();
+
+    productArrayList.add(new Product("Table", 400));
+    productArrayList.add(new Product("Chair", 200));
+    productArrayList.add(new Product("Tablet", 300));
+    productArrayList.add(new Product("TV", 400));
+    productArrayList.add(new Product("Phone", 300));
+    productArrayList.add(new Product("keyboard", 100));
+
+    List<String> results = solution.findSecondLineProducts(productArrayList);
+
+
+    Assert.assertEquals(List.of("Tablet", "Phone"), results);
   }
 
   @Test(expected = RuntimeException.class)
